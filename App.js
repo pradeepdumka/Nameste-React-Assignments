8a68ad73cfa6939352ef4839181858fabe252170
Pradeep Dumka <57614710+PradeepKmDumka@users.noreply.github.com>
2023-01-03 22:48:16 +0530

    done assignment-1

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+  
 
-let heading1 = React.createElement('h1',{
-    id:"heading1",
-    key:'h1'
-},'This is Heading - 1') ;
+ const div = React.createElement('div',{
+    className:'title'
+ },
+ [
+    React.createElement('h1',{key:7},'This is  Heading H1.'),
+    React.createElement('h2',{key:8},'This is  Heading H2.'),
+    React.createElement('h3',{key:9},'This is  Heading H3.'),
+ ]
+ ) 
 
-let heading2 = React.createElement('h2',{
-    id:"headllling2",
-    key:'h2'
-},'This is Heading 3chh') ;
+ const NestedHeading = (<div className='title'>
+    <h1 key={1}>This is Heading 1 Using JSX </h1>
+    <h2  key={2}>This is Heading 1 Using JSX </h2>
+    <h3  key={3}>This is Heading 1 Using JSX </h3>
+ </div>)
 
-let div = React.createElement('div',{
-    id:"container"
-},[heading1,heading2]) ;
 
-let root =  ReactDOM.createRoot(document.getElementById('root'));
-root.render(div)
+const HeaderComponent = ()=>{
+    return (
+        <div className='header'>
+            <div className='logo'> <a>Logo#</a></div>
+            <div className='menu'>
+                <ul>
+                    <li key='4'><a href="">Home</a></li>
+                    <li key='5'><a href="">About</a></li>
+                    <li key='6'><a href="">Contact US</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const MenuHeader = ()=>{
+    return (
+        <ul>
+            <li key='4'><a href="">Home</a></li>
+            <li key='5'><a href="">About</a></li>
+            <li key='6'><a href="">Contact US</a></li>
+        </ul>
+        )
+ }
+
+
+const HeaderComponent1 = ()=>(
+        <div className='header'>
+            <div className='logo'> <a>Logo#</a></div>
+            <div className='menu'>
+               {MenuHeader()}
+            </div>
+        </div>
+);
+
+
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ const jsxRoot = ReactDOM.createRoot(document.getElementById('jsx-root'));
+
+ const header = ReactDOM.createRoot(document.getElementById('header'));
+
+ root.render(div);
+ jsxRoot.render(NestedHeading);
+ header.render(<HeaderComponent1 />);
