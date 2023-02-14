8a14069b269fe8c7537efa3c6cb1bb88b8810d56
Pradeep Dumka <57614710+PradeepKmDumka@users.noreply.github.com>
2023-02-14 21:43:54 +0530

    changes

--- src/Constant.js
+++ src/Constant.js
@@ -1,5 +1,5 @@
 export const IMG_CDN_URL ="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
-export const API_CDN_URL ="https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4594965&lng=77.0266383&page_type=DESKTOP_WEB_LISTING";
+export const API_CDN_URL ="https://63ebb25d311a001fc22ac9c5--ephemeral-gaufre-6bbeab.netlify.app/dapi/restaurants";
 export const RESTRAU_DETAILS_URL = "https://www.swiggy.com/dapi/menu/v4/full?lat=28.4594965&lng=77.0266383&menuId=";
 
  
